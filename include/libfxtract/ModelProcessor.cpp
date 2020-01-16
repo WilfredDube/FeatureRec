4e0acb3e28a3e61209a2bd5f9feb7dcc67a15b14
unbusy <unbusy@lhost.com>
2020-01-16 22:51:19 +0200

    Added an Abstract Processor class

---
+++ include/libfxtract/ModelProcessor.cpp
@@ -0,0 +1,5 @@
+#include "ModelProcessor.h"
+
+void ModelProcessor::addFace(ModelFace& face) {  vface.push_back(face); }
+void ModelProcessor::addTopoDSFace(TopoDS_Face face) {  tvfaces.push_back(face); }
+std::vector<ModelFace> ModelProcessor::getModelFaces(){ return vface; }
---
+++ include/libfxtract/ModelProcessor.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <TopoDS_Shape.hxx>
+#include <BRepAdaptor_Curve.hxx>
+#include <TopoDS_Edge.hxx>
+#include <TopoDS_Face.hxx>
+#include <TopoDS_Vertex.hxx>
+#include <TopExp_Explorer.hxx>
+#include "ModelMath.h"
+#include "ModelFace.h"
+
+using namespace std;
+
+class ModelProcessor
+{
+  std::vector<ModelFace> vface;
+  std::vector<TopoDS_Face> tvfaces;
+
+public:
+  virtual void extractFeactures(XSControl_Reader reader) = 0;
+
+  std::vector<ModelFace> getModelFaces();
+  void addFace(ModelFace& face);
+  void addTopoDSFace(TopoDS_Face face);
+};
