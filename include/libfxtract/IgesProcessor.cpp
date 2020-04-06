d9d742d238681c1acb26a83f341563e7e5c8f78a
unbusy <unbusy@lhost.com>
2020-04-06 20:24:44 +0200

    Clean up and removed inheritance

--- include/libfxtract/IgesProcessor.cpp
+++ include/libfxtract/IgesProcessor.cpp
@@ -1,40 +1,24 @@
 #include "IgesProcessor.h"
+#include "ModelUtils.h"
 
 namespace Fxt {
 
-  void IgesProcessor::extractFeactures(XSControl_Reader reader) {
-    size_t p = 0, nfaces = 0, nbends = 0;
+  void IgesProcessor::extractFaces(Model& model_, const XSControl_Reader reader)
+  {
+    if (reader.NbShapes() <= 0)
+      throw invalid_argument("Unable to read model.");
 
-    Standard_Integer nbs = reader.NbShapes();
+    FaceID faceID = 0;
+    Standard_Integer numShapes = reader.NbShapes();
 
     /* Go through the shape object and extract the faces. */
-    for (int i = 1; i <= nbs; i++) {
-      TopoDS_Shape ashape = reader.Shape(i);
-      TopoDS_Face& face = static_cast<TopoDS_Face&>(TopoDS::Face(ashape));
-      ModelFace* test;
+    for (size_t i = 1; i <= numShapes; i++) {
+      TopoDS_Shape aShape = reader.Shape(i);
 
-      ++p;
-      Standard_Real curvature = compute_curvature(face);
-      if (curvature == 0.0){
-        ++nfaces;
-        test = new ModelFace(p, PlaneType::PLANAR);
-      } else {
-        ++nbends;
-        test = new ModelFace(p, PlaneType::NON_PLANAR);
-      }
+      ++faceID;
 
-      test->setUnitNormal(compute_unit_normal(face));
-      test->setCurvature(curvature);
-      test->extractEdges(face);
-      test->setBendLength(test->getFaceEdges());
-      test->computeFaceNormal();
-      test->computeFaceEquation();
-      // test->printUnitNormal();
-      addFace(*test);
-      addTopoDSFace(face);
+      model_.assignFaceAttributes(faceID, aShape);
     }
-
-    std::cout << "Number of faces : " << nfaces << " Number of bends: " << nbends << '\n';
   }
 
 }
--- include/libfxtract/IgesProcessor.h
+++ include/libfxtract/IgesProcessor.h
@@ -1,17 +1,22 @@
 #ifndef IGES_PROCESSOR_H_
 #define IGES_PROCESSOR_H_
 
-#include <IGESControl_Reader.hxx>
-#include "ModelProcessor.h"
+#include <XSControl_Reader.hxx>
+#include "Model.h"
 
 using namespace std;
 
 namespace Fxt {
 
-  class IgesProcessor : public ModelProcessor
+  class IgesProcessor
   {
   public:
-    void extractFeactures(XSControl_Reader reader);
+    /**
+    *   Extracts all the TopoDS_Face objects from a step file model
+    *   @param model_ the current model from which to extract TopoDS_Faces
+    *   @param reader a IGESControl_Reader object
+    */
+    void extractFaces(Model& model_, const XSControl_Reader reader);
   };
 }
 #endif // IGES_PROCESSOR_H_
