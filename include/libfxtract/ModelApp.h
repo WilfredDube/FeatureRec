62ba158820ecedd93b7d56d24001db74d02411c5
unbusy <unbusy@lhost.com>
2020-04-06 20:22:57 +0200

    CLean up

--- include/libfxtract/ModelApp.h
+++ include/libfxtract/ModelApp.h
@@ -1,39 +1,10 @@
 #ifndef MODEL_APP_H_
 #define MODEL_APP_H_
 
-#include <iostream>
-#include <memory>
-#include <cstring>
 #include <string>
-#include <IGESControl_Reader.hxx>
-#include <XSControl_Reader.hxx>
-#include <TColStd_HSequenceOfTransient.hxx>
-#include <TopoDS_Shape.hxx>
-#include <BRep_Tool.hxx>
-#include <BRepTools.hxx>
-#include <BRepAdaptor_Surface.hxx>
-#include <BRepAdaptor_Curve.hxx>
-#include <BRepLib_MakeWire.hxx>
-#include <GeomConvert.hxx>
-#include <Geom_Surface.hxx>
-#include <Geom_BSplineSurface.hxx>
-#include <GeomLProp_SLProps.hxx>
-#include <BRepLProp_SLProps.hxx>
-#include <TopoDS.hxx>
-#include <TopoDS_Edge.hxx>
-#include <TopoDS_Face.hxx>
-#include <TopoDS_Vertex.hxx>
-#include <TopExp_Explorer.hxx>
-#include <Interface_Static.hxx>
-#include <ShapeFix_Shape.hxx>
-#include <ShapeFix_Wireframe.hxx>
-#include <ShapeFix_Face.hxx>
-#include <ShapeAnalysis_Edge.hxx>
-#include <ShapeFix_Edge.hxx>
-#include <ShapeBuild_ReShape.hxx>
-#include <algorithm>
+
 #include "../libfxtract/ModelMath.h"
-#include "../libfxtract/ModelFace.h"
+#include "../libfxtract/Model.h"
 #include "../libfxtract/StepProcessor.h"
 #include "../libfxtract/IgesProcessor.h"
 #include "../libfxtract/ModelReader.h"
@@ -42,7 +13,6 @@
 
 #include <Wt/WApplication.h>
 #include <Wt/Dbo/Dbo.h>
-// #include "../include/testtopo.h"
 
 /*
 gcc -Wall -I /usr/local/include/opencascade/ -L/usr/local/lib/ -lTKIGES -lTKSTEP -lTKXSBase -lTKBO -lTKBRep -lTKBool -lTKFeat -lTKFillet -lTKG2d -lTKG3d -lTKGeomAlgo -lTKGeomBase -lTKHLR -lTKMath -lTKMesh -lTKOffset -lTKPrim -lTKShHealing -lTKTopAlgo -lTKXMesh -lTKernel -lstdc++ -std=c++1y testmain.cpp -o test -lTKTopAlgo -lTKBRep -lTKernel -ltk -ltcl -lGL -ltbb -lXmu -lgl2ps -lm -Wl,-R/usr/local/lib
@@ -55,14 +25,12 @@ namespace Fxt {
   static const std::string DB_FOLDER = "../../src/";
 
   class ModelApp {
-    // dbo::SqlConnectionPool& connectionPool_;
     Session& session_;
   public:
-    // ModelApp(dbo::SqlConnectionPool& connectionPool);
     ModelApp(Session& session);
+
     ~ModelApp();
 
-    // static
     int run(char const *modelFile, dbo::ptr<ModelFile> p);
   };
 }
