efab616ca10914b4dc2d807c2b18c3b478a6b9c7
unbusy <unbusy@lhost.com>
2020-04-06 20:03:54 +0200

    Replaced by the Model Class

--- include/libfxtract/ModelProcessor.cpp
+++
@@ -1,8 +0,0 @@
-#include "ModelProcessor.h"
-
-namespace Fxt {
-
-  void ModelProcessor::addFace(ModelFace& face) {  vface.push_back(face); }
-  void ModelProcessor::addTopoDSFace(TopoDS_Face face) {  tvfaces.push_back(face); }
-  std::vector<ModelFace> ModelProcessor::getModelFaces(){ return vface; }
-}
