bd7d213847852a8a86c68f21fddb4d8f695c6415
unbusy <unbusy@lhost.com>
2020-04-06 19:27:02 +0200

    Code restructed to remove unnecessary inheritance. Added comments

--- include/libfxtract/StepProcessor.h
+++ include/libfxtract/StepProcessor.h
@@ -1,17 +1,22 @@
 #ifndef STEP_PROCESSOR_H_
 #define STEP_PROCESSOR_H_
 
-#include <STEPControl_Reader.hxx>
-#include "ModelProcessor.h"
+#include <XSControl_Reader.hxx>
+#include "Model.h"
 
 using namespace std;
 
 namespace Fxt {
 
-  class StepProcessor : public ModelProcessor
+  class StepProcessor
   {
   public:
-    void extractFeactures(XSControl_Reader reader);
+    /**
+    *   Extracts all the TopoDS_Face objects from a step file model
+    *   @param model_ the current model from which to extract TopoDS_Faces
+    *   @param reader a STEPControl_Reader object
+    */
+    void extractFaces(Model& model_, const XSControl_Reader reader);
   };
 
 }
